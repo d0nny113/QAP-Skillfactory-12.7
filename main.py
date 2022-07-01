009b5f9b487ed8856976cc214e5ba34ba57ee8c8
d0nny113 <d0nny13051990@gmail.com>
2022-07-02 01:15:08 +0530

    update input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = float(input("Введите сумму депозита:"))
+money = float(input("Введите сумму депозита: "))
 deposit = list(per_cent.values())
 for i in range(len(deposit)):
     deposit[i] = money / 100 * deposit[i]
